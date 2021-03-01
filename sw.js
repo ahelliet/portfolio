9bc06273a75ed4f19d677aaa00a70d720e1109c4
Anthony <ahelliet@gmail.com>
2021-03-01 01:27:24 +0100

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f2ee38af4f1fde640472.js"
+    "url": "webpack-runtime-36ccb4090ecde21180c3.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d41926369cb04d997f540d4ad39251c4"
+    "revision": "bda22c53e5768275cf6606304f09173f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-1972652a375cec723dd9.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "0f9ffa68be169adf1f8e6fe409e91794"
+    "revision": "dc618b1eeac390cbb1164e4e9fc3f61d"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
